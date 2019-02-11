9c4441d0ddc842ee2afd5b83038b5b6c769623f9
ishland <ishlandmc@yeah.net>
2019-02-11 14:13:49 +0800

    Format codes and modify Metrics

--- src/main/java/com/ishland/bukkit/AsyncKeepAlive/main/AsyncKeepAlive.java
+++ src/main/java/com/ishland/bukkit/AsyncKeepAlive/main/AsyncKeepAlive.java
@@ -7,22 +7,20 @@ package com.ishland.bukkit.AsyncKeepAlive.main;
 import java.util.Set;
 import java.util.TreeSet;
 import java.util.UUID;
-import java.util.HashMap;
-import java.util.Map;
 
+import org.bstats.bukkit.Metrics;
 import org.bukkit.Bukkit;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.junit.rules.Timeout;
-import org.bstats.bukkit.Metrics;
 
 import com.comphenix.protocol.PacketType;
 import com.comphenix.protocol.ProtocolLibrary;
 import com.comphenix.protocol.ProtocolManager;
 import com.comphenix.protocol.events.ListenerPriority;
 import com.comphenix.protocol.events.PacketAdapter;
-import com.comphenix.protocol.events.PacketEvent;
 import com.comphenix.protocol.events.PacketContainer;
+import com.comphenix.protocol.events.PacketEvent;
 import com.comphenix.protocol.reflect.StructureModifier;
 
 /**
@@ -38,10 +36,11 @@ public class AsyncKeepAlive extends JavaPlugin implements Listener {
     }
 
     public Set<UUID> inTimeout = new TreeSet<UUID>();
+    private Metrics metrics;
 
     @Override
     public void onEnable() {
-	Metrics metrics = new Metrics(this);
+	setMetrics(new Metrics(this));
 	protocolManager = ProtocolLibrary.getProtocolManager();
 	getLogger().info("AsyncKeepAlive by ishland");
 	if (Bukkit.getPluginManager().getPlugin("ProtocolLib") == null) {
@@ -50,25 +49,25 @@ public class AsyncKeepAlive extends JavaPlugin implements Listener {
 	    return;
 	}
 	if (!(Bukkit.getVersion().contains("1.13.2") || Bukkit.getVersion().contains("1.12.2")))
-		getLogger().warning("Minecraft " + Bukkit.getVersion() + " hasn't been tested yet!");
+	    getLogger().warning("Minecraft " + Bukkit.getVersion() + " hasn't been tested yet!");
 	try {
 	    new AsyncPacketThread().start();
 	    protocolManager.addPacketListener(
 		    new PacketAdapter(this, ListenerPriority.HIGHEST, PacketType.Play.Client.KEEP_ALIVE) {
 			@Override
 			public void onPacketReceiving(PacketEvent e) {
-                            try {
-			        PacketContainer keepAlivePacket = e.getPacket();
-                                StructureModifier<Long> packetData = keepAlivePacket.getLongs();
-                                Long packetValue = packetData.readSafely(0);
-                                getLogger().fine("Got keepalive " + String.valueOf(packetValue));
-                                if(packetValue == 0L) {
-                                    e.setCancelled(true);
-                                }
-                            } catch (Throwable t) {
-                                getLogger().warning("Caught a exception");
-                                t.printStackTrace();
-                            }
+			    try {
+				PacketContainer keepAlivePacket = e.getPacket();
+				StructureModifier<Long> packetData = keepAlivePacket.getLongs();
+				Long packetValue = packetData.readSafely(0);
+				getLogger().fine("Got keepalive " + String.valueOf(packetValue));
+				if (packetValue == 0L) {
+				    e.setCancelled(true);
+				}
+			    } catch (Throwable t) {
+				getLogger().warning("Caught a exception");
+				t.printStackTrace();
+			    }
 			}
 		    });
 	} catch (Throwable t) {
@@ -82,5 +81,13 @@ public class AsyncKeepAlive extends JavaPlugin implements Listener {
     public void onDisable() {
 	getLogger().warning("Current version of AsyncKeepAlive cannot be disabled completely.");
 	getLogger().info("AsyncKeepAlive 0.1-SNAPSHOT is now Disabled!");
+    }
+
+    public Metrics getMetrics() {
+	return metrics;
+    }
+
+    public void setMetrics(Metrics metrics) {
+	this.metrics = metrics;
     }
 }
