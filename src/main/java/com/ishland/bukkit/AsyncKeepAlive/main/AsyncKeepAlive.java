d5f77dca2897a7d953461db3d3e30a4532cbf0cb
ishland <ishlandmc@yeah.net>
2019-03-01 21:42:26 +0800

    Back to snapshot and use AsyncManager

--- src/main/java/com/ishland/bukkit/AsyncKeepAlive/main/AsyncKeepAlive.java
+++ src/main/java/com/ishland/bukkit/AsyncKeepAlive/main/AsyncKeepAlive.java
@@ -10,10 +10,17 @@ import java.util.UUID;
 
 import org.bstats.bukkit.Metrics;
 import org.bukkit.Bukkit;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.junit.rules.Timeout;
 
+import com.comphenix.protocol.PacketType;
 import com.comphenix.protocol.ProtocolLibrary;
+import com.comphenix.protocol.events.ListenerPriority;
+import com.comphenix.protocol.events.PacketAdapter;
+import com.comphenix.protocol.events.PacketContainer;
+import com.comphenix.protocol.events.PacketEvent;
+import com.comphenix.protocol.reflect.StructureModifier;
 
 /**
  * @author ishland
@@ -21,8 +28,9 @@ import com.comphenix.protocol.ProtocolLibrary;
  */
 public class AsyncKeepAlive extends JavaPlugin {
     public static Timeout main;
-    private AsyncReceiveThread ReceiveThread;
-    private AsyncPacketThread PacketThread;
+    private Runnable ReceiveThread;
+    private Runnable PacketThread;
+    private FileConfiguration configuration;
 
     public static Timeout getInstance() {
 	return main;
@@ -34,34 +42,64 @@ public class AsyncKeepAlive extends JavaPlugin {
     @Override
     public void onEnable() {
 	setMetrics(new Metrics(this));
-	ProtocolLibrary.getProtocolManager();
 	getLogger().info("AsyncKeepAlive by ishland");
+
 	if (Bukkit.getPluginManager().getPlugin("ProtocolLib") == null) {
 	    getLogger().warning("You need ProtocolLib in order to use this plugin");
 	    getServer().getPluginManager().disablePlugin(this);
 	    return;
 	}
 	if (!(Bukkit.getVersion().contains("1.13") || Bukkit.getVersion().contains("1.12")
-		|| Bukkit.getVersion().contains("1.11")))
+		|| Bukkit.getVersion().contains("1.11") || Bukkit.getVersion().contains("1.10")
+		|| Bukkit.getVersion().contains("1.7.10")))
 	    getLogger().warning("Minecraft " + Bukkit.getVersion() + " hasn't been tested yet!");
 	try {
 	    setPacketThread(new AsyncPacketThread());
-	    getPacketThread().setPlugin(this);
-	    getPacketThread().start();
-	    setReceiveThread(new AsyncReceiveThread());
-	    getReceiveThread().setPlugin(this);
-	    getReceiveThread().run();
+	    ((AsyncPacketThread) getPacketThread()).setPlugin(this);
+	    runThread(getPacketThread());
+	    ProtocolLibrary.getProtocolManager().getAsynchronousManager().registerAsyncHandler(
+		    new PacketAdapter(this, ListenerPriority.HIGHEST, PacketType.Play.Client.KEEP_ALIVE) {
+			@Override
+			public void onPacketReceiving(PacketEvent e) {
+			    if (e.getPacketType() == PacketType.Play.Client.KEEP_ALIVE) {
+				try {
+				    PacketContainer keepAlivePacket = e.getPacket();
+				    if (Bukkit.getVersion().contains("1.13") || Bukkit.getVersion().contains("1.12")) {
+					StructureModifier<Long> packetData = keepAlivePacket.getLongs();
+					Long packetValue = packetData.readSafely(0);
+					// System.out.println("Got keepalive " + String.valueOf(packetValue));
+					if (packetValue == 0L) {
+					    e.setCancelled(true);
+					}
+				    } else {
+					StructureModifier<Integer> packetData = keepAlivePacket.getIntegers();
+					int packetValue = packetData.readSafely(0);
+					// System.out.println("Got keepalive " + String.valueOf(packetValue));
+					if (packetValue == 0L) {
+					    e.setCancelled(true);
+					}
+				    }
+				} catch (Throwable t) {
+				    System.out.println("Caught a exception");
+				    System.out.println(t.getMessage());
+				    t.printStackTrace();
+				}
+			    }
+			}
+		    }).start();
 	} catch (Throwable t) {
 	    t.printStackTrace();
 	    getServer().getPluginManager().disablePlugin(this);
+	    return;
 	}
-	getLogger().info("AsyncKeepAlive 0.2 is now Enabled!");
+	getLogger().info("AsyncKeepAlive 0.2.1-SNAPSHOT is now Enabled!");
     }
 
     @Override
     public void onDisable() {
-	getLogger().warning("Current version of AsyncKeepAlive cannot be disabled completely.");
-	getLogger().info("AsyncKeepAlive 0.2 is now Disabled!");
+	ProtocolLibrary.getProtocolManager().removePacketListeners(this);
+	((AsyncPacketThread) getPacketThread()).doStop();
+	getLogger().info("AsyncKeepAlive 0.2.1-SNAPSHOT is now Disabled!");
     }
 
     public Metrics getMetrics() {
@@ -72,19 +110,38 @@ public class AsyncKeepAlive extends JavaPlugin {
 	this.metrics = metrics;
     }
 
-    public AsyncReceiveThread getReceiveThread() {
+    public Runnable getReceiveThread() {
 	return ReceiveThread;
     }
 
-    public void setReceiveThread(AsyncReceiveThread receiveThread) {
+    public void setReceiveThread(Runnable receiveThread) {
 	ReceiveThread = receiveThread;
     }
 
-    public AsyncPacketThread getPacketThread() {
+    public Runnable getPacketThread() {
 	return PacketThread;
     }
 
-    public void setPacketThread(AsyncPacketThread packetThread) {
+    public void setPacketThread(Runnable packetThread) {
 	PacketThread = packetThread;
     }
+
+    public void runThread(Runnable runnable) {
+	new Thread(runnable).start();
+    }
+
+    /**
+     * @return the configuration
+     */
+    public FileConfiguration getConfiguration() {
+	return configuration;
+    }
+
+    /**
+     * @param configuration the configuration to set
+     */
+    public void setConfiguration(FileConfiguration configuration) {
+	this.configuration = configuration;
+    }
+
 }
--- src/main/java/com/ishland/bukkit/AsyncKeepAlive/main/AsyncPacketThread.java
+++ src/main/java/com/ishland/bukkit/AsyncKeepAlive/main/AsyncPacketThread.java
@@ -11,7 +11,6 @@ import java.util.concurrent.TimeUnit;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
-import org.junit.rules.Timeout;
 
 import com.comphenix.protocol.PacketType;
 import com.comphenix.protocol.ProtocolLibrary;
@@ -22,19 +21,19 @@ import com.comphenix.protocol.events.PacketContainer;
  * @author ishland
  *
  */
-public class AsyncPacketThread extends Thread {
-    public static Timeout main;
+public class AsyncPacketThread implements Runnable {
     private ProtocolManager protocolManager;
     private Plugin plugin;
+    private boolean doStop = false;
 
-    public AsyncPacketThread() {
-	setDaemon(true);
+    public void doStop() {
+	doStop = true;
     }
 
     public void run() {
 	protocolManager = ProtocolLibrary.getProtocolManager();
 	getPlugin().getLogger().info("Packet thread started.");
-	for (;;) {
+	for (; !doStop;) {
 	    try {
 		Iterator<?> localIterator = Bukkit.getServer().getOnlinePlayers().iterator();
 		while (localIterator.hasNext()) {
@@ -55,6 +54,7 @@ public class AsyncPacketThread extends Thread {
 	    } catch (InterruptedException localInterruptedException) {
 	    }
 	}
+	getPlugin().getLogger().info("Packet thread stopped.");
     }
 
     public Plugin getPlugin() {
